a39c4ce3809a04275f58218affa87b81398feacf
Onur Koçak <98240992+onurkck34@users.noreply.github.com>
2022-03-22 22:15:09 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+class Matematik:
+    def __init__(self,sayi1,sayi2):
+        self.sayi1 = sayi1
+        self.sayi2 = sayi2
+        
+    def topla(self):
+        return self.sayi1 + self.sayi2
+    
+    def cikar(self):
+        return self.sayi1 - self.sayi2
+    
+    def carp(self):
+        return self.sayi1 * self.sayi2
+    
+    def bol(self):
+        return self.sayi1 / self.sayi2
+    
+matematik = Matematik(2,78)
+matematik2 = Matematik(3,76)
+print("Toplam = " + str(matematik2.topla()))
